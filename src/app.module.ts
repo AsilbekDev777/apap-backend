078672d7ef98f429aa51e7373741cb7151bf1701
Asilbek <asilturdiyev777@gmail.com>
2026-04-28 23:41:09 +0500

    chore: docker + production config + SSL support

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,15 +25,20 @@ import { AdminModule } from './modules/admin/admin.module';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
+        url: process.env.DATABASE_URL,
         host: config.get<string>('database.host'),
         port: config.get<number>('database.port'),
         username: config.get<string>('database.username'),
         password: config.get<string>('database.password'),
         database: config.get<string>('database.database'),
+        ssl:
+          process.env.NODE_ENV === 'production'
+            ? { rejectUnauthorized: false }
+            : false,
         entities: [__dirname + '/database/entities/*.entity{.ts,.js}'],
         migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
         synchronize: false,
-        logging: true,
+        logging: process.env.NODE_ENV !== 'production',
       }),
     }),
     EventEmitterModule.forRoot(),
